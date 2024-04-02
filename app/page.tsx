b4f978378b4401299e0daf94448e2877a615a5f4
heekang2271 <hk.shin2271@gmail.com>
2024-04-02 17:08:33 +0900

    has, peer등 선택자들 사용

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 export default function Home() {
   return (
     <main className="bg-gray-100 h-screen sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 flex items-center justify-center p-5">
-      <div className="bg-white shadow-lg p-5 rounded-2xl w-full max-w-screen-sm flex flex-col gap-2 md:flex-row">
+      <div className="bg-white shadow-lg p-5 rounded-2xl w-full max-w-screen-sm flex flex-col gap-2 md:flex-row *:outline-none has-[:invalid]:ring-red-100 has-[:invalid]:ring">
         <input
-          className="w-full rounded-full h-10 bg-gray-200 pl-5 py-6 outline-none ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
+          className="w-full rounded-full h-10 bg-gray-200 pl-5 py-6 ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
           type="email"
           required
           placeholder="Email address"
         />
         <span className="text-red-500 font-medium hidden peer-invalid:block">Email is required.</span>
-        <button className="bg-black text-white py-2 rounded-full active:scale-90 focus:scale-90 transition-transform font-medium outline-none md:px-10 peer-invalid:bg-red-100">
+        <button className="bg-black text-white py-2 rounded-full active:scale-90 focus:scale-90 transition-transform font-medium md:px-10 peer-invalid:bg-red-100">
           Log in
         </button>
       </div>
